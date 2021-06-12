88a95aff18baca90612591860d23a4ac482706f3
Andrey.Makhnev <ray.makhnev@gmail.com>
2021-06-12 16:11:46 +0300

    add editorcfg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,15 @@ import '../styles/globals.css';
 import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-	return (
-		<>
-			<Head>
-				<title>MyTop - наш лучший топ</title>
-				<link ref="icon" href="/favicon.ico" />
-			</Head>
-			<Component {...pageProps} />
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link ref="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
