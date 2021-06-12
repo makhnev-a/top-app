923677d6b4f8f65fabb37f14bf41a9af4880384c
Andrey.Makhnev <ray.makhnev@gmail.com>
2021-06-12 16:07:39 +0300

    add header & document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/globals.css';
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>MyTop - наш лучший топ</title>
+				<link ref="icon" href="/favicon.ico" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp
